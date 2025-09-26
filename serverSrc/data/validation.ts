e7b35d96704742c83c3a9a7fcffef28a8ad34b07
Sara Serti <sara.serti@outlook.com>
2025-09-26 12:11:41 +0200

    remove id from interface

--- serverSrc/data/validation.ts
+++ serverSrc/data/validation.ts
@@ -3,7 +3,7 @@ import type { CartItem, ErrorMessage } from './types.js'
 
 
 const CartSchema = z.object({
-  id: z.string(),
+ 
   userId: z.string().min(1).max(50),
   productId: z.string().regex(/^p\d+$/), 
   amount: z.number().int().min(1).max(10),
--- serverSrc/routes/cart.ts
+++ serverSrc/routes/cart.ts
@@ -1,6 +1,6 @@
 import type { Request, Response, Router } from 'express';
 import express from 'express';
-import { GetCommand, ScanCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import db from '../data/dynamodb.js';
 import { myTable } from '../data/dynamodb.js';
@@ -9,6 +9,7 @@ import {CartSchema, isCartItem} from "../data/validation.js"
 
 
 const router: Router = express.Router();
+
 
 
 
