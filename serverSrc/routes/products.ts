65219006fd2150c08f7767398daa18f99633abe3
Julia Hagstedt <juliahagstedt@hotmail.com>
2025-09-26 09:51:20 +0200

    Latest from dev and Julia branch

--- serverSrc/routes/products.ts
+++ serverSrc/routes/products.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import type { Request, Response, Router } from 'express';
 import * as z from "zod"
 import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
 import db from '../data/dynamodb.js';
 import { myTable } from '../data/dynamodb.js';
 import type { Product, ErrorMessage } from '../data/types.js';
@@ -10,8 +10,23 @@ import type { Product, ErrorMessage } from '../data/types.js';
 
 const router: Router = express.Router();
 
+const client = new DynamoDBClient({  
+    region: "eu-north-1",
+    credentials: {
+    accessKeyId: process.env.ACCESS_KEY!,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY!
+  }});
 
 
+router.get("/", async (req, res) => {
+  try {
+    const command = new ScanCommand({ TableName: "Products" });
+    const data = await client.send(command);
+    res.status(200).json(data.Items);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch products" });
+  }
+});
 
 
 
