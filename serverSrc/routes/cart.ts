05816c770ae1c8f81af20527a29651fdb8e79d18
Sara Serti <sara.serti@outlook.com>
2025-09-26 10:51:19 +0200

    changes

--- serverSrc/routes/cart.ts
+++ serverSrc/routes/cart.ts
@@ -10,28 +10,6 @@ import {CartSchema, isCartItem} from "../data/validation.js"
 
 const router: Router = express.Router();
 
-router.get('/', async (req, res) => {
-	const result = await db.send(new ScanCommand({
-		TableName: myTable
-	}))
-
-	if( result.Count === undefined || result.Items === undefined ) {
-		res.sendStatus(500)  
-		return
-	}
-
-	let parseResult = CartSchema.safeParse(result.Items)
-	if( !parseResult.success ) {
-		console.log( result.Items, parseResult.error)
-		res.sendStatus(500)
-		return
-	}
-
-	// const items: (CartItem | undefined) = parseResult.data
-	// const filtered: CartItem = items.filter(isCartItem)
-	
-	// res.send(filtered)
-})
 
 
-export default Router;
+export default router;
