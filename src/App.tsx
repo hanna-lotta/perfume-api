5a3946ab507c743975a1ee60d89def638cf93598
Hanna Selden <hanna_lotta@hotmail.com>
2025-09-27 13:14:53 +0200

    products frontend css

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,23 @@ function App() {
 		const data = await response.json()
 		setProducts(data)
 	}
+	/*
+	const handleAddProduct = async (productData: any) => {
+		const uniqueId = crypto.randomUUID()
+		const response = await fetch(`/api/products/${uniqueId}`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(productData)
+		})
+		
+		if (response.ok) {
+			const newProduct = await response.json()
+			setProducts(prev => [...prev, newProduct]) // Lägg till utan reload
+		}
+	}*/
 
 	
+
 	useEffect(() => {
 		handleGetProducts()
 	}, [])
@@ -21,10 +36,15 @@ function App() {
   return (
     <div>
 		<h1>Parfymer</h1>
+		<div>
+			<input type="text" 
+			placeholder="Sök parfym..." />
+		</div>
 		<div className="products-grid">
 			{products.map((product: Product) => 
 				<div className='product-card' key={product.Sk}>
 					<h3>{product.name}</h3>
+					<img src={product.img} />
 					<p>{product.price} kr</p>
 					<p> ✔️ {product.amountInStock} i lager</p>
 				</div>
